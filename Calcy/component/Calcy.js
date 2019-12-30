16feb908fb15456d03234e4ebae01f4339cd1dae
shubham860 <shubham.chauhan860@gmail.com>
2019-12-31 00:13:25 +0530

    commit-2

--- Calcy/component/Calcy.js
+++ Calcy/component/Calcy.js
@@ -1,14 +1,48 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 
 class Calcy extends Component {
     render() {
         return (
             <View style={css.container}>
-                <View style={css.display}></View>
+                <View style={css.display}>
+                    <View style={css.calculation}>
+                        <Text style={css.calculationString}>11*11</Text>
+                    </View>
+                    <View style={css.result}>
+                        <Text style={css.resultString}>121</Text>
+                    </View>
+                </View>
                 <View style={css.buttons}>
-                    <View style={css.numbers}></View>
-                    <View style={css.operations}></View>
+                    <View style={css.numbers}>
+                        <View style={css.row}>
+                            <Button title='0' onPress={this.handler} />
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                        </View>
+                        <View style={css.row}>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                        </View>
+                        <View style={css.row}>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                        </View>
+                        <View style={css.row}>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                            <Button title='0' onPress={this.handler}/>
+                        </View>
+
+                    </View>
+                    <View style={css.operations}>
+                        <Button title='0' onPress={this.handler}/>
+                        <Button title='0' onPress={this.handler}/>
+                        <Button title='0' onPress={this.handler}/>
+                        <Button title='0' onPress={this.handler}/>
+                    </View>
                 </View>
             </View>
         );
@@ -25,20 +59,50 @@ const css = StyleSheet.create({
        backgroundColor : 'white',
    },
 
+    calculation : {
+      flex : 2,
+      justifyContent : 'flex-end',
+      alignItems:  'flex-end'
+    },
+
+    result : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems:  'flex-end'
+    },
+
     buttons : {
         flex : 2,
         backgroundColor: 'black',
-        flexDirection : 'row'
+        flexDirection : 'row',
+        justifyContent: 'space-between'
     },
 
     numbers : {
        flex : 3,
-        backgroundColor : 'orange'
+        backgroundColor : 'orange',
    },
 
    operations : {
-       flex : 1
-   }
+       flex : 1,
+       justifyContent : 'space-around'
+   },
+
+   row : {
+       flex : 1,
+       flexDirection: 'row',
+       justifyContent : 'space-around',
+       alignItems : 'center',
+   },
+
+    calculationString : {
+       fontSize : 56,
+       fontWeight : "600"
+    },
+
+    resultString : {
+       fontSize: 40
+    }
 
 
 });
